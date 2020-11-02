20ca5d18a34f6f59d6f6d6229501d06c8af31c56
Christoph Witzko <github@christophwitzko.com>
2020-11-02 21:42:17 +0100

    chore: add start-gitea script

---
+++ scripts/start-gitea.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker stop gitea || true
+docker rm gitea || true
+
+git checkout ./test/gitea/gitea.db
+
+docker run -d --name gitea -p 3000:3000 \
+  -e USER_UID=1000 -e USER_GID=1000 \
+  -v $(pwd)/test/gitea/conf/:/data/gitea/conf/ \
+  -v $(pwd)/test/gitea/gitea.db:/data/gitea/gitea.db \
+  gitea/gitea
+
+sleep 5
+
+echo "creating test repo..."
+curl -u 'test:test' -XPOST -H 'Content-Type: application/json' -d '{"name":"test"}' http://localhost:3000/api/v1/user/repos
