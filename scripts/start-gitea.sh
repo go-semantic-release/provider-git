5da822baa935549f0f5914429c9f07a6c5598bd3
Christoph Witzko <github@christophwitzko.com>
2022-01-20 09:59:45 +0100

    ci: pin gitea version

--- scripts/start-gitea.sh
+++ scripts/start-gitea.sh
@@ -11,9 +11,9 @@ docker run -d --name gitea -p 3000:3000 \
   -e USER_UID=1000 -e USER_GID=1000 \
   -v $(pwd)/test/gitea/conf/:/data/gitea/conf/ \
   -v $(pwd)/test/gitea/gitea.db:/data/gitea/gitea.db \
-  gitea/gitea
+  gitea/gitea:1.15.6
 
-sleep 5
+sleep 10
 
 echo "creating test repo..."
 curl -u 'test:test' -XPOST -H 'Content-Type: application/json' -d '{"name":"test"}' http://localhost:3000/api/v1/user/repos
